2b4e802caf82e6608d5784e209b917119c931bec
alanhakhyeonsong <songs4805@naver.com>
2022-07-06 13:30:03 +0900

    Init: dev settings

---
+++ src/main/java/ramos/InCalifornia/InCaliforniaApplication.java
@@ -0,0 +1,13 @@
+package ramos.InCalifornia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InCaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InCaliforniaApplication.class, args);
+	}
+
+}
