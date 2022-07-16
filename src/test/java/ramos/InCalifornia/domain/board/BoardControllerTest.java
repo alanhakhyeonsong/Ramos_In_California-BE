3263d5b5e1a282b65993bb2ca4e2bc95ed83a15f
alanhakhyeonsong <songs4805@naver.com>
2022-07-16 18:53:45 +0900

    Test: 게시글 CRUD unit test

--- src/test/java/ramos/InCalifornia/domain/board/BoardControllerTest.java
+++ src/test/java/ramos/InCalifornia/domain/board/BoardControllerTest.java
@@ -24,11 +24,12 @@ import java.util.Map;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static ramos.InCalifornia.support.BoardTestHelper.givenBoard;
+import static ramos.InCalifornia.support.BoardTestHelper.givenUpdateBoard;
 
 
 @WebMvcTest(controllers = BoardController.class)
@@ -84,8 +85,41 @@ public class BoardControllerTest {
     }
 
     // 수정
+    @Test
+    @DisplayName("게시글 수정 성공")
+    @WithMockAuthUser(id = 1L, email = "test@test.com", role = Role.ROLE_ADMIN)
+    void editBoardSuccess() throws Exception {
+        //given
+        Map<String, String> input = new HashMap<>();
+        input.put("title", "게시글 제목 수정 테스트");
+        input.put("contents", "게시글 본문 수정 테스트");
+
+        BoardResponse result = givenUpdateBoard();
+
+        given(boardService.edit(any(Long.class), any(EnrollRequest.class), any(AuthMember.class))).willReturn(result);
+
+        //andExpect
+        mockMvc.perform(
+                put("/board/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(input)))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
 
     // 삭제
+    @Test
+    @DisplayName("게시글 삭제 성공")
+    void deleteBoardSuccess() throws Exception {
+        //given
+        doNothing().when(boardService).delete(any(Long.class), any(AuthMember.class));
+
+        //andExpect
+        mockMvc.perform(
+                delete("/board/{id}", 1L))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
 
     // 페이징 목록 조회
 }
